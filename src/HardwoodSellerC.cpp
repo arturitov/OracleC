c3d0abb7322f70979dd726c09269f6f5c6d06e11
arturitov <aav13@my.fsu.edu>
2016-01-27 20:37:40 -0500

    Reading Input Data
    Filled out the function that reads the input data and created a struct to hold that data. Instead of having the function provided return void I changed it to return a struct object with all the information in the file

--- src/HardwoodSellerC.cpp
+++ src/HardwoodSellerC.cpp
@@ -7,20 +7,64 @@
 //============================================================================
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include "WoodItem.h"
 using namespace std;
 
+struct customer_data
+{
+	string name;
+	string address;
+	string date;
+	std::vector<string> wood;
+};
+
+customer_data readInputFile(string);
+
 int main() {
-	cout
-			<< "This is an empty main method"
-			<< endl;
+	
+	string inputFile = "input.txt";
+	customer_data customer;
+	customer = readInputFile(inputFile);
+
 	return 0;
 }
 
 /*
  * Method to read the input file
  */
-void readInputFile(string inputFilePath) {
+customer_data readInputFile(string inputFilePath) {
 
+	ifstream myfile (inputFilePath);
+	string line;
+	string wd;
+	string qty;
+
+	customer_data customer;
+	int run = 0;
+	while(getline(myfile,line))
+	{
+	    std::stringstream   linestream(line);
+	    std::string         value;
+	    if (run == 0)
+	    {
+	    	getline(linestream,customer.name,';');
+	    	getline(linestream,customer.address,';');
+	    	getline(linestream,customer.date,';');
+	    }
+	    else{
+	    	while(getline(linestream,wd,':') && getline(linestream,qty,';'))
+	    	{
+		    	customer.wood.push_back(wd);
+		    	customer.wood.push_back(qty);
+	    	}
+	    }
+	    run++;
+	}
+	return customer;
 }
 
 /*
